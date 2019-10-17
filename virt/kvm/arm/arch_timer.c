9933461b36ab019f3b3fac7f13eccc7009bcf903
Shannon Zhao <shannon.zhao@linux.alibaba.com>
2019-10-17 12:15:54 +0800

    KVM: arch_timer: Fix resource leak on error path
    It needs to cleanup irq setup for host_vtimer_irq when
    request_percpu_irq fails for host_ptimer_irq. It needs to cleanup irq
    setup for both host_vtimer_irq and host_ptimer_irq when seeting vcpu
    affinity error as well.
    
    Fixes: 9e01dc76be6a ("KVM: arm/arm64: arch_timer: Assign the phys timer on VHE systems")
    Signed-off-by: Shannon Zhao <shannon.zhao@linux.alibaba.com>

--- virt/kvm/arm/arch_timer.c
+++ virt/kvm/arm/arch_timer.c
@@ -960,7 +960,7 @@ int kvm_timer_hyp_init(bool has_gic)
 		if (err) {
 			kvm_err("kvm_arch_timer: can't request ptimer interrupt %d (%d)\n",
 				host_ptimer_irq, err);
-			return err;
+			goto out_free_irq;
 		}
 
 		if (has_gic) {
@@ -968,7 +968,7 @@ int kvm_timer_hyp_init(bool has_gic)
 						    kvm_get_running_vcpus());
 			if (err) {
 				kvm_err("kvm_arch_timer: error setting vcpu affinity\n");
-				goto out_free_irq;
+				goto out_free_pirq;
 			}
 		}
 
@@ -984,6 +984,8 @@ int kvm_timer_hyp_init(bool has_gic)
 			  "kvm/arm/timer:starting", kvm_timer_starting_cpu,
 			  kvm_timer_dying_cpu);
 	return 0;
+out_free_pirq:
+	free_percpu_irq(host_ptimer_irq, kvm_get_running_vcpus());
 out_free_irq:
 	free_percpu_irq(host_vtimer_irq, kvm_get_running_vcpus());
 	return err;
